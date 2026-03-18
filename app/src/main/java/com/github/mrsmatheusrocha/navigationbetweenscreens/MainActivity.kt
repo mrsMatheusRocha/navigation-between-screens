4c225ac485849d3d2a82a54eed7942b6021ee67b
Matheus Rocha Sousa <166670004+mrsMatheusRocha@users.noreply.github.com>
2026-03-18 00:01:56 -0300

    Implementação da interface inicial da tela de Login

--- app/src/main/java/com/github/mrsmatheusrocha/navigationbetweenscreens/MainActivity.kt
+++ app/src/main/java/com/github/mrsmatheusrocha/navigationbetweenscreens/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.github.mrsmatheusrocha.navigationbetweenscreens.screens.LoginScreen
 import com.github.mrsmatheusrocha.navigationbetweenscreens.ui.theme.NavigationBetweenScreensTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavigationBetweenScreensTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    LoginScreen()
                 }
             }
         }
